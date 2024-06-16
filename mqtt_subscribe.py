475480c72933645011c4737d58daaa122ea55596
Matt Sheppard <msheppard@beta.team>
2024-06-16 11:32:28 -0400

    first steps towards MQTT based picocom implementation

---
+++ mqtt_subscribe.py
@@ -0,0 +1,32 @@
+import time
+from umqtt.simple import MQTTClient
+import ubinascii
+import machine
+
+# MQTT Server Parameters
+MQTT_BROKER = "broker.hivemq.com"
+CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+MQTT_TOPIC = b"com.mampersat/neopixel/1"
+
+# Callback when a message is received
+def sub_cb(topic, msg):
+    print((topic, msg))
+
+# Setup MQTT Client and Subscribe
+def mqtt_subscribe():
+    client = MQTTClient(CLIENT_ID, MQTT_BROKER)
+    client.set_callback(sub_cb)
+    client.connect()
+    client.subscribe(MQTT_TOPIC)
+    print("Connected to %s, subscribed to %s topic" % (MQTT_BROKER, MQTT_TOPIC))
+    
+    try:
+        while True:
+            client.wait_msg()  # Blocking wait for a message
+    except KeyboardInterrupt:
+        pass
+    finally:
+        client.disconnect()
+
+# Run the MQTT subscription
+mqtt_subscribe()
