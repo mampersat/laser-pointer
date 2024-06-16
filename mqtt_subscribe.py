b346d4585c18a4571f9cc843a10aa8ea248e734b
Matt Sheppard <msheppard@beta.team>
2024-06-16 11:55:31 -0400

    mqtt processing driving neopixel color

--- mqtt_subscribe.py
+++ mqtt_subscribe.py
@@ -1,7 +1,9 @@
 import time
 from umqtt.simple import MQTTClient
 import ubinascii
+import json
 import machine
+import neopixel
 
 # MQTT Server Parameters
 MQTT_BROKER = "broker.hivemq.com"
@@ -9,9 +11,19 @@ CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 #MQTT_TOPIC = b"com.mampersat/neopixel/1"
 MQTT_TOPIC = b"com/mampersat/mousecoords"
 
+np = neopixel.NeoPixel(machine.Pin(28), 5)
+
 # Callback when a message is received
 def sub_cb(topic, msg):
     print((topic, msg))
+    payload_dict = json.loads(msg)
+    x = payload_dict['x']
+    y = payload_dict['y']
+    np[1] = (x, y, 0)
+    np.write()
+            
+
+
 
 # Setup MQTT Client and Subscribe
 def mqtt_subscribe():
